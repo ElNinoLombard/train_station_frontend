377203407e1e12c916dc554d3b5339b6e9169b4d
MaximeLombard <112693934+MaximeLombard@users.noreply.github.com>
2023-06-16 15:59:19 +0200

    CA

--- src/app/features/ceo/ceo.component.ts
+++ src/app/features/ceo/ceo.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { TrainService } from 'src/app/shared/train.service';
 
 @Component({
   selector: 'app-ceo',
   templateUrl: './ceo.component.html',
-  styleUrls: ['./ceo.component.scss']
+  styleUrls: ['./ceo.component.scss'],
 })
 export class CeoComponent implements OnInit {
+  ca: number = 0;
 
-  constructor() { }
+  constructor(private trainService: TrainService) {}
 
   ngOnInit(): void {
+    this.getRevenue();
   }
 
+  private getRevenue(): void {
+    this.trainService.getCA().subscribe({
+      next: (response) => {
+        console.log('Revenue fetched:', response);
+        this.ca = response.data;
+      },
+      error: (error) => {
+        console.log('Error fetching revenue:', error);
+      },
+    });
+  }
 }
